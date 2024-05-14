f2863a6790ce049403e0ab8a7463428c44e0f53c
AliciaNazar <alicianazarab@gmail.com>
2024-05-14 19:03:54 -0300

    cambios pequeños

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, OnInit, inject } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { ServiceService } from '../../services/service.service';
 import { slideFromBottom,slide1,slide2 } from '../../animations';
@@ -14,17 +14,40 @@ import { ContactoComponent } from "../contacto/contacto.component";
     animations: [slideFromBottom, slide1, slide2],
     imports: [CommonModule, ContactoComponent]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   
   valor = inject(ServiceService);
- 
+  
 
 
   expandido: boolean = false;
+  positions: { x: number, y: number }[] = [];
+  positions2: { x: number, y: number }[] = [];
+
+ngOnInit() {
+  for (let i = 0; i < 50; i++) {
+    this.positions.push(this.generateRandomPosition());
+  }
+  for (let i = 0; i < 50; i++) {
+    this.positions2.push(this.generateRandomPosition());
+  }
+}
+
 
   expandir() {
     this.expandido = !this.expandido;
+  }
+  
+
+  generateRandomPosition(): { x: number, y: number } {
+    const maxWidth = document.body.clientWidth; 
+    const maxHeight = document.body.clientHeight; 
+  
+    const x = Math.random() * (maxWidth - 100);
+    const y = Math.random() * (maxHeight - 100);
+  
+    return { x, y };
   }
   
 }
